e8eb1f509c4af03551ab2c3c0ff6c3920312a956
Tyler Hilliard <tylerghilliard@gmail.com>
2022-05-25 16:01:55 -0500

    day two collab

--- app_api/views/Movie.py
+++ app_api/views/Movie.py
@@ -1,3 +1,4 @@
+from os import stat
 from django.http import HttpResponseServerError
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
@@ -11,20 +12,32 @@ class MovieView(ViewSet):
     def list(self, request):
         movies = Movie.objects.all()
 
-        genre_id = self.request.query_params.get('genre_id', None)
+        genre_id = request.query_params.get('genre_id', None)
         if genre_id is not None:
             movies = movies.filter(genre__id=genre_id)
         serializer = MovieSerializer(movies, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def create(self, request):
-        pass
+        serializer = CreateMovieSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(user=request.auth.user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def update(self, request, pk):
-        pass
+        movie = Movie.objects.get(pk=pk)
+        serializer = CreateMovieSerializer(movie, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
 
     def destroy(self, request, pk):
-        pass
+        movie = Movie.objects.get(pk=pk)
+        if movie.user == request.auth.user:
+            movie.delete()
+            return Response(None, status=status.HTTP_204_NO_CONTENT)
+        else:
+            return Response("You can not delete this.", status=status.HTTP_204_NO_CONTENT)
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -34,12 +47,18 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['username', 'first_name']
 
 
+class CreateMovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = ("title", "description", "run_time", "date_released", "genre")
+
+
 class MovieSerializer(serializers.ModelSerializer):
 
     user = UserSerializer()
 
     class Meta:
         model = Movie
-        fields = ("title", "description", "run_time",
+        fields = ("id", "title", "description", "run_time",
                   "user", "date_released", "genre")
         depth = 1
